fb5e4e6c53e41dc1fa7d7a6257c9df1a3f9af382
Josh Sullivan <josh@dipperstove.com>
2011-05-14 12:14:16 -0700

    Initial commit.
    Add .DS_Store and *.swp to .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G5radio::Application.config.secret_token = '32ceeebc5a7a60e24838dc6de026200d7bdba7d85fe3ee2524160e3f660f451ee47f2e76d39c7a549dd36be4c53ad573774241edb647a8e874b4f44cc14fbe8b'
