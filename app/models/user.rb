f139c4c47a4df60ed1ae9148e96f8ac0370e646e
Josh Sullivan <josh@dipperstove.com>
2011-05-14 15:51:32 -0700

    Add avatar to user create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["user_info"]["name"]
+      user.screen_name = auth["user_info"]["nickname"]
+      user.avatar = auth["user_info"]["image"]
     end
   end
 end
